af35fa8f8b38a686892e83039cf0fdff8a1cc898
laerne <wesnoth@projectiveplane.net>
2016-02-22 15:17:40 +0100

    Remove dictionary list checkboxes.  Code for edit dictionary button.  Forced usage of UTF-8.

--- Loader.py
+++ Loader.py
@@ -19,23 +19,24 @@ from utilities import warn, fail
 
 def loadDictionary( *filepaths, weightTransformationFct=(lambda x: x) ):
     dictionary = {}
-    for line in fileinput.input( filepaths ):
-        item = line.split('#')[0].strip()
-        tokens = item.split(':')
-        word = tokens[0]
-        if word == '':
-            continue
-        try:
-            weight = float(tokens[1]) if len( tokens ) > 2 else 1
-        except ValueError as e:
-            from utilities import warn
-            warn( "Incorrect weight value.  Defaulting to 1." )
-            weight = 1
-        weight = weightTransformationFct( weight )
-        if word in dictionary:
-            dictionary[word] += weight
-        else:
-            dictionary[word] = weight
+    with fileinput.input( filepaths, openhook=fileinput.hook_encoded("UTF-8") ) as lines:
+        for line in lines:
+            item = line.split('#')[0].strip()
+            tokens = item.split(':')
+            word = tokens[0]
+            if word == '':
+                continue
+            try:
+                weight = float(tokens[1]) if len( tokens ) > 2 else 1
+            except ValueError as e:
+                from utilities import warn
+                warn( "Incorrect weight value.  Defaulting to 1." )
+                weight = 1
+            weight = weightTransformationFct( weight )
+            if word in dictionary:
+                dictionary[word] += weight
+            else:
+                dictionary[word] = weight
     return dictionary
 
 
