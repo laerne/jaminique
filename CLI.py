3783b144d0a23380a5f3f30491ea29fa33346787
laerne <wesnoth@projectiveplane.net>
2016-03-08 01:13:58 +0100

    Support for markov added + revamp generator code organization.

--- CLI.py
+++ CLI.py
@@ -16,9 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Jaminique.  If not, see <http://www.gnu.org/licenses/>.
 
-import PrefixCounter
 import Loader
-import Generator
+import SmoothMarkov
+import Markov
 import argparse
     
 if __name__ != '__main__':
@@ -29,51 +29,84 @@ argparser = argparse.ArgumentParser(
         description='Generate a random name from a flavor of existing names.'
         )
 
+#general options
 argparser.add_argument( '-n', '--number', action='store', type=int, default=1,
-        help='number of names to generate' )
+        help="number of names to generate" )
 argparser.add_argument( '-u', '--uniform', action='store_true', default=False,
-        help='ignore possible word weight and set them all to 1' )
-argparser.add_argument( '-s', '--ngram-size', action='store', type=int, default=2,
-        help='how many previous characters are looked to choose the next one (default 2)' )
+        help="ignore possible word weight and set them all to 1" )
+argparser.add_argument( '-i', '--ignore-case', action='store_true', default=False,
+        help="ignore case of dictionary files (generate all lowercase words)" )
 argparser.add_argument( '-l', '--min-length', action='store', type=int, default=3,
-        help='minimun length of a generated name (default 3)' )
+        help="minimun length of a generated name (default 3)" )
 argparser.add_argument( '-L', '--max-length', action='store', type=int, default=20,
-        help='maximum length of a generated name (default 20)' )
-argparser.add_argument( '-o', '--original', action='store_true', default=False,
-        help='discard words already existing in the dictionary' )
+        help="maximum length of a generated name (default 20)" )
 argparser.add_argument( '-p', '--min-perpexity', action='store', type=float, default=0.0,
-        help='tolerance threshold to discard words that matches too closely the examples (default 0.0)' )
+        help="tolerance threshold to discard words that matches too closely the examples (default 0.0)" )
 argparser.add_argument( '-P', '--max-perpexity', action='store', type=float, default=10,
-        help='tolerance threshold to discard ugly words (default 10.0)' )
+        help="tolerance threshold to discard ugly words (default 10.0)" )
+argparser.add_argument( '-o', '--original', action='store_true', default=False,
+        help="discard words already existing in the dictionary" )
 argparser.add_argument( '-S', '--seed', action='store', type=int,
-        help='seed for the random generator.  If this option is not provided, the current system time is used as a seed.' )
+        help="seed for the random generator.  If this option is not provided, the current system time is used as a seed." )
+argparser.add_argument( '-v', '--verbose', action='store_true', default=False,
+        help="print additional information like the perplexity." )
+argparser.add_argument( '-a', '--algo', metavar = 'ALGO', action='store', default='smooth-markov',
+        choices=['smooth-markov','markov'],
+        help="""algorithm used to generate the names.
+        Possible choices include 'markov', 'smooth-markov' (default "markov").""" )
+        
+#markov options
+argparser.add_argument( '--markov-ngram-size', metavar='SIZE', action='store', type=int, default=3,
+        help="In the 'markov' algorihm, set how many previous characters are looked to choose the next one (default 3)" )
+        
+#smooth markov options
+argparser.add_argument( '--smooth-markov-ngram-size', metavar='SIZE', action='store', type=int, default=2,
+        help="In the 'smooth-markov' algorihm, set how many previous characters are looked to choose the next one (default 2)" )
+        
+#non-options arguments (dictionary files)
 argparser.add_argument( 'dictionary', metavar='FILE', action='store', nargs='*',
         default=['namelists/viking_male.txt','namelists/viking_female.txt','namelists/viking_unknownGender.txt'],
-        help='Dictonary files to learn names from' )
+        help='dictonary files to learn names from' )
 
 args = argparser.parse_args()
 
 if args.seed:
     import random
     random.seed(args.seed)
-dictionary = Loader.loadDictionary( *args.dictionary )
-prefixCounter = PrefixCounter.PrefixCounter( dictionary, minlength=0, maxlength=args.ngram_size, generateDelimiterSymbols=True )
-generator = Generator.SimpleLagrangeGenerator( prefixCounter, minlength = args.min_length, maxlength = args.max_length )
+dictionary = Loader.loadDictionary( *args.dictionary, forceLowerCase = args.ignore_case )
+
+if args.algo == 'markov':
+    generator = Markov.Generator( 
+            dictionary,
+            nGramLength = 3,
+            generateDelimiterSymbols = True,
+            minNameLength = args.min_length,
+            maxNameLength = args.max_length )
+elif args.algo == 'smooth-markov':
+    generator = SmoothMarkov.Generator( 
+            dictionary,
+            minNGramLength = 0,
+            maxNGramLength = args.smooth_markov_ngram_size,
+            generateDelimiterSymbols = True,
+            minNameLength = args.min_length,
+            maxNameLength = args.max_length )
+else:
+    raise Exception("Invalid generator name given.")
 
 n = 0
 while n < args.number:
     namePerplexity, name = generator.generateName()
-    if args.original and (name in dictionary) :
-        pass
-        #print("%.6f!: %s" % (namePerplexity, name) )
-    elif namePerplexity > args.max_perpexity:
-        pass
-        #print("%.6f>: %s" % (namePerplexity, name) )
-    elif namePerplexity < args.min_perpexity:
-        pass
-        #print("%.6f<: %s" % (namePerplexity, name) )
+    if args.verbose and args.original and (name in dictionary) :
+        print("%.6f!: %s" % (namePerplexity, name) )
+    elif args.verbose and namePerplexity > args.max_perpexity:
+        print("%.6f>: %s" % (namePerplexity, name) )
+    elif args.verbose and namePerplexity < args.min_perpexity:
+        print("%.6f<: %s" % (namePerplexity, name) )
     else:
-        print("%.6f :: %s" % (namePerplexity, name) )
+        if args.verbose:
+            print("%.6f :: %s" % (namePerplexity, name) )
+        else:
+            print("%s" % name )
         n +=1
 
 
---
+++ Markov.py
@@ -0,0 +1,153 @@
+# This file is part of Jaminique.
+# Copyright (C) 2016 by Nicolas BRACK <nicolas.brack@mail.be>
+# 
+# Jaminique is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Jaminique is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Jaminique.  If not, see <http://www.gnu.org/licenses/>.
+
+from utilities import discretepick
+
+class TransitionTable(object):
+    def __init__( self,
+            dictionary,
+            length = 2,
+            generateDelimiterSymbols = True,
+            ):
+        assert type(length) == int
+        assert type(generateDelimiterSymbols) == bool
+
+        self.length_ = length
+        self.characters_ = set()
+        
+        self.table_ = buildTransitionTable(
+                dictionary=dictionary,
+                length=length,
+                generateDelimiterSymbols=generateDelimiterSymbols,
+                encounteredCharacters= self.characters_
+                )
+        self.characters_ = sorted( self.characters_ )
+    
+    def characters( self ):
+        yield from self.characters_
+    
+    def transitionsForPrefix( self, prefix ):
+        assert type(prefix) == str
+        
+        if prefix not in self.table_:
+            return None
+        yield from sorted( self.table_[ prefix ].items() )
+
+    def prefixScore( self, prefix ):
+        assert type(prefix) == str
+        
+        if prefix not in self.table_:
+            return 0
+        s = sum( self.table_[prefix].values() )
+        return s
+    
+    def length( self ):
+        return self.length_
+        
+    def printCountTable( self ):
+        print( "     ", "|".join( map(normalizeStr,self.characters()) ) )
+        print( "-----", "+".join( map(lambda e:"----",self.characters()) ) )
+        for p in sorted(self.table_):
+            print( normalizeStr(p), end='||' )
+            for c in self.characters():
+                if c in self.table_[p]:
+                    v = self.table_[p][c]
+                    print( "% 4d"%(v,), end='|' )
+                else:
+                    print(" "*4, end='|')
+            print()
+
+def normalizeStr( c, l=4 ):
+    c = repr(c)[1:-1][:l]
+    c = " "*(l-len(c)) + c
+    return c
+
+
+def buildTransitionTable(
+        dictionary,
+        length = 2,
+        generateDelimiterSymbols = True,
+        encounteredCharacters = None ):
+
+    prefixCounters = {}
+    for word, weight in dictionary.items():
+        if generateDelimiterSymbols:
+            word = '^' + word + '$'
+        ngram = ''
+        for c in word:
+            if encounteredCharacters != None:
+                encounteredCharacters.add( c )
+                ngram += c
+                if len(ngram) > length:
+                    ngram = ngram[1:]
+
+                prefix = ngram[:-1]
+                character = ngram[-1]
+                if character == '^':
+                    continue
+                if prefix not in prefixCounters:
+                    prefixCounters[ prefix ] = {}
+                if character not in prefixCounters[ prefix ]:
+                    prefixCounters[ prefix ][ character ] = 0
+                prefixCounters[ prefix ][ character ] += weight
+    return prefixCounters
+
+class Generator(object):
+    def __init__(
+            self,
+            dictionary,
+            nGramLength = 2,
+            generateDelimiterSymbols = True,
+            minNameLength=3,
+            maxNameLength=24 ):
+
+        self.nGramLength_ = nGramLength
+        self.transitionTable_ = TransitionTable( dictionary,
+                length = nGramLength+1,
+                generateDelimiterSymbols = generateDelimiterSymbols )
+        self.minNameLength_ = minNameLength
+        self.maxNameLength_ = maxNameLength
+        
+    def generateName( self ):
+        name = '^'
+        probabilityOfName = 1.0
+        while len(name) < self.maxNameLength_ + 1:
+            prefix = name[-self.nGramLength_:] if self.nGramLength_ > 0 else ''
+            #prefixScore = self.transitionTable_.prefixScore( prefix )
+            
+            transitions = list( self.transitionTable_.transitionsForPrefix( prefix ) )
+            if not transitions:
+                self.transitionTable_.printCountTable()
+                break
+            transitionCharacters, transitionScores = zip( *transitions )
+            
+            i = discretepick( transitionScores )
+            character = transitionCharacters[i]
+            
+            if character == '$':
+                if len(name) < self.minNameLength_:
+                    continue
+                else:
+                    break
+
+            name += character
+            probabilityOfName *= transitionScores[i] / sum(transitionScores)
+            
+        
+        name=name[1:]
+        namePerplexity = (probabilityOfName**(-1/len(name)))
+        
+        return (namePerplexity, name)
